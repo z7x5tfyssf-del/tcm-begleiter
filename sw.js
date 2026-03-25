90595eaf6472e910be19a3bb287999e9b7d94fa7
z7x5tfyssf-del <z7x5tfyssf@privaterelay.appleid.com>
2026-03-25 19:57:41 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE_NAME = 'tcm-v3';
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); });
